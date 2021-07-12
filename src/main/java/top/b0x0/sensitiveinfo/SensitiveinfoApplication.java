12387d1ac0e2cba338fd2d57ac8fc98612ca5131
tlh <vlingl@163.com>
2021-07-12 13:21:59 +0800

    Initial commit

---
+++ src/main/java/top/b0x0/sensitiveinfo/SensitiveinfoApplication.java
@@ -0,0 +1,13 @@
+package top.b0x0.sensitiveinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensitiveinfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensitiveinfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/b0x0/sensitiveinfo/SensitiveinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.b0x0.sensitiveinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensitiveinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
